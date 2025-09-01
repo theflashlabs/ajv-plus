806232080e91f8745d5ad2719924366667a45f29
Balakrishna Avulapati <ba@bavulapati.com>
2025-09-01 08:48:03 +0530

    add back the whitespace to minimize the changes
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- spec/ajv.ts
+++ spec/ajv.ts
@@ -1,5 +1,6 @@
 import type Ajv from "../dist/core"
 const AjvClass: typeof Ajv = typeof window == "object" ? (window as any).ajv7 : require("" + "..")
+
 export default AjvClass
 module.exports = AjvClass
 module.exports.default = AjvClass
