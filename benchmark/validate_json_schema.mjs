b9368f8b942e12ed5be9ab1570b1a412af46ff0a
Balakrishna Avulapati <ba@bavulapati.com>
2025-09-03 17:23:21 +0530

    Add benchmarks
    Benchmark using benchmark.js and
    https://github.com/benchmark-action/github-action-benchmark
    
    The schemas are borrowed from
    https://github.com/sourcemeta-research/jsonschema-benchmark/tree/main/schemas
    
    closes #26
    
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

---
+++ benchmark/validate_json_schema.mjs
@@ -0,0 +1,52 @@
+import fs from "fs"
+import readline from "readline"
+import addFormats from "ajv-formats"
+
+const DRAFTS = {
+  "https://json-schema.org/draft/2020-12/schema": (await import("../dist/2020.js")).Ajv2020,
+  "https://json-schema.org/draft/2019-09/schema": (await import("../dist/2019.js")).Ajv2019,
+  "http://json-schema.org/draft-07/schema": (await import("../dist/ajv.js")).Ajv,
+}
+
+function readJSONFile(filePath) {
+  try {
+    const fileContent = fs.readFileSync(filePath, "utf8")
+    return JSON.parse(fileContent)
+  } catch (error) {}
+}
+
+async function* readJSONLines(filePath) {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(filePath),
+  })
+  for await (const line of rl) {
+    yield JSON.parse(line)
+  }
+}
+
+function validateAll(instances, validator) {
+  let failed = false
+  for (const instance of instances) {
+    if (!validator(instance)) {
+      failed = true
+    }
+  }
+
+  return failed
+}
+
+export default async function validateSchema(schemaPath, instancePath) {
+  const schema = readJSONFile(schemaPath)
+
+  const ajv = new DRAFTS[schema["$schema"].replace(/#$/, "")]({strict: false, allErrors: true})
+  addFormats(ajv)
+
+  const validate = ajv.compile(schema)
+
+  const instances = []
+  for await (const instance of readJSONLines(instancePath)) {
+    instances.push(instance)
+  }
+
+  validateAll(instances, validate)
+}
